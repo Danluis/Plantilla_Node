3043acf3d5cc339e5676f36c19b318a1076a72f2
Danluis <danluis2a25@gmail.com>
2024-01-21 21:27:47 -0400

    Creacion de modelo-controlador-ruta de user

---
+++ app/controllers/user.js
@@ -0,0 +1,21 @@
+const getItems = () => {
+
+}
+
+const getItem = () => {
+    
+}
+
+const createItem = () => {
+    
+}
+
+const updateItem = () => {
+    
+}
+
+const deleteItem = () => {
+    
+}
+
+module.exports = { getItem, getItems, createItem, updateItem, deleteItem }
