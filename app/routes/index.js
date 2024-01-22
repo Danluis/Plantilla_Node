09c95849fb4870a8ba4f37fb1b5e1705a3712841
Danluis <danluis2a25@gmail.com>
2024-01-21 22:05:27 -0400

    configuracion ruta no encontrada (404)

--- app/routes/index.js
+++ app/routes/index.js
@@ -17,4 +17,9 @@ fs.readdirSync(pathRouter).filter((file) => {
     }
 })
 
+router.get('*', (req, res) => {
+    res.status(404)
+    res.send({ error: 'Not Found' })
+})
+
 module.exports = router
